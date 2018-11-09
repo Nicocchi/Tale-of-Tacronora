27aa4fc8f04734b60e3ba80653e49297a879dbf2
Nicocchi <nicocchilover@gmail.com>
2018-11-08 22:24:57 -0500

    added job class and jobs dict

---
+++ src/job.py
@@ -0,0 +1,8 @@
+class Job:
+    def __init__(self, name, attack, vitality, intelligence, dexterity, wisdom):
+        self.name = name
+        self.attack = attack
+        self.vitality = vitality
+        self.intelligence = intelligence
+        self.dexterity = dexterity
+        self.wisdom = wisdom
---
+++ src/jobs.py
@@ -0,0 +1,7 @@
+from job import *
+
+jobs = {
+    'Warrior': Job('Warrior', 28, 10, 10, 15, 5),
+    'Mage': Job('Mage', 25, 15, 25, 10, 25),
+    'Thief': Job('Thief', 15, 20, 10, 25, 5),
+}
